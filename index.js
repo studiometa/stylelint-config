fb6f49db8fe4df5393db8cbaa64de8c780df6130
Titouan Mathis <titouan@studiometa.fr>
2022-08-30 14:36:46 +0200

    Always use Prettier

--- index.js
+++ index.js
@@ -1,25 +1,27 @@
 module.exports = {
   customSyntax: 'postcss-scss',
-  extends: [require.resolve('./rules/property-order.js'), require.resolve('./rules/selector.js')],
+  extends: [
+    'stylelint-config-recommended',
+    'stylelint-config-recommended-scss',
+    require.resolve('./rules/property-order.js'),
+    require.resolve('./rules/selector.js'),
+    'stylelint-config-prettier',
+  ],
   plugins: ['stylelint-high-performance-animation'],
   rules: {
-    indentation: [2, { baseIndentLevel: 1 }],
     'plugin/no-low-performance-animation-properties': [true, { ignore: 'paint-properties' }],
-    'max-line-length': 100,
-    'at-rule-empty-line-before': [
-      'always',
-      {
-        ignore: ['first-nested', 'after-comment'],
-        ignoreAtRules: ['return', 'warn', 'import', 'else'],
-      },
-    ],
     'scss/at-rule-no-unknown': [
       true,
       {
         ignoreAtRules: ['tailwind', 'apply', 'variants', 'responsive', 'screen'],
       },
     ],
-    'block-opening-brace-space-before': 'always',
-    'declaration-colon-space-after': 'always',
+    'rule-empty-line-before': [
+      'always',
+      {
+        except: ['after-single-line-comment'],
+        ignore: ['after-comment', 'first-nested'],
+      },
+    ],
   },
 };
--- prettier.js
+++
@@ -1,3 +0,0 @@
-module.exports = {
-  extends: [require.resolve('./index.js'), 'stylelint-prettier/recommended'],
-};
